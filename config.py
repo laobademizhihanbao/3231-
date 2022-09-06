35e2bbfc73ae2eecf3c5233e75896d7fb0324ab7
laobademizhihanbao <112808296+laobademizhihanbao@users.noreply.github.com>
2022-09-07 00:44:21 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+{
+# 公众号配置
+# 公众号appId
+"app_id": "wx66d8a3471293592f",
+# 公众号appSecret
+"app_secret": "7aa8fb2945bb280858abe5168cfb7d4a",
+# 模板消息id
+"template_id": "qz5Dp5GrG7vK0I9aOQQRldMONpqtu6fcbCPd-Dq1giE",
+# 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
+"user": ["omSdn64XSGxH8uaaO75c6XK0-uwk"],
+ 
+# 信息配置
+# 和风天气apikey
+"weather_key": "db464adfef894ab3a5870926d038b89a",
+# 所在地区，可为省，城市，区，县，同时支持国外城市，例如伦敦
+"region": "保定市",
+# 生日1，修改名字为对应需要显示的名字，如果生日为农历，在最前面加上r即可
+"birthday1": {"name": "微微", "birthday": "2000-06-14"},
+# 生日2
+"birthday2": {"name": "##", "birthday": "2001-01-01"},
+# 在一起的日子，格式同上，暂不不支持农历
+"love_date": "2014-09-10",
+# 金句中文，如果设置了，则会显示这里的，如果为空，默认会读取金山的每日金句
+"note_ch": "",
+# 金句英文
+"note_en": ""
+}
